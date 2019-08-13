8b50930de970a26e34ba66803720517b382f3423
clayton-scherer <cscherer93@aol.com>
2019-08-13 17:11:16 -0400

    react app bootstrapped

---
+++ client/src/components/RecipeCard/RecipeCard.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const RecipeCard = () => {
+  return (
+    <div className="recipe-card">
+      <h2 className="recipe__name">dish</h2>
+    </div>
+  );
+};
+export default RecipeCard;
--- readme.md
+++ readme.md
@@ -1,1 +1,5 @@
 # Flover
+
+# Not sure what to eat? Flover is here for you!
+
+## Match with delicious looking dishes on Flover and check your matches to see their recipes.
