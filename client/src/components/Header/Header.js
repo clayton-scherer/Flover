ea46cc67f0d4d9e5e1dd011a4c13698424821006
clayton-scherer <cscherer93@aol.com>
2019-08-13 18:59:19 -0400

    created header component for layout, separated stylesheets

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../../stylesheets/Header.css"
+const Header = () => {
+  return (
+    <header className="main-header">
+      <h1 className="main-header__title">Welcome to Flover</h1>
+    </header>
+  );
+};
+export default Header;
