acf7b7ca8a937266222c7413b6f1058795440443
clayton-scherer <cscherer93@aol.com>
2019-08-26 17:02:06 -0400

    develop incrementation thru entree queue

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Layout from "./components/Layout/Layout";
 import EntreeCard from "./components/EntreeCard/EntreeCard";
 import "./stylesheets/App.css";
 
 const App = () => {
-  // const [entrees, setEntrees] = useState([]);
-  // useEffect(() => {
-  //   fetch("http://localhost:3000/api/entrees")
-  //     .then(res => res.json())
-  //     .then(entrees => setEntrees(entrees));
-  // }, []);
+  const [entrees, setEntrees] = useState([]);
+  const [initialEntree, setInitialEntree] = useState({
+    name: "PB&J",
+    ingredients: ["Bread", "Peanut Butter", "Jelly"],
+    cookTime: "1 minute",
+    pictureURL: `https://static.smuckersrms.com/PhotoImage.ashx?recipeid=8492&w=600&h=600`
+  });
 
-  const handlePassIncriment = () => {
+  useEffect(() => {
+    fetch("http://localhost:3000/api/entrees")
+      .then(res => res.json())
+      .then(entrees => setEntrees(entrees));
+  }, []);
+
+  const handlePassIncrement = () => {
     console.log("passed");
+    setInitialEntree(entrees[0]);
   };
-  const handleLikeIncriment = () => {
+  const handleLikeIncrement = () => {
     console.log("liked");
+    setInitialEntree(entrees[1]);
   };
 
   return (
     <Layout>
       <EntreeCard
-        handlePassIncriment={handlePassIncriment}
-        handleLikeIncriment={handleLikeIncriment}
+        initialEntree={initialEntree}
+        handlePassIncrement={handlePassIncrement}
+        handleLikeIncrement={handleLikeIncrement}
       />
     </Layout>
   );
--- client/src/components/EntreeCard/EntreeCard.jsx
+++ client/src/components/EntreeCard/EntreeCard.jsx
@@ -1,27 +1,35 @@
 import React from "react";
 import "../../stylesheets/EntreeCard.css";
 
-const EntreeCard = ({ handlePassIncriment, handleLikeIncriment }) => {
+const EntreeCard = ({
+  handlePassIncrement,
+  handleLikeIncrement,
+  initialEntree
+}) => {
   const style = {
-    backgroundImage: `url(https://assets.kraftfoods.com/recipe_images/opendeploy/52089_640x428.jpg)`
+    backgroundImage: initialEntree.pictureURL
   };
   return (
     <div className="entree-card__wrapper">
       <article className="entree-card">
-        <h2 className="entree__name">entree name</h2>
+        <h2 className="entree__name">{initialEntree.name}</h2>
         <span className="entree__picture" style={style} />
-        <h3 className="entree__cookTime">Cook Time: 20 min</h3>
-        <h3 className="entree__ingredients">Ingredients: number of items</h3>
+        <h3 className="entree__cookTime">
+          Cook Time: {initialEntree.cookTime}
+        </h3>
+        <h3 className="entree__ingredients">
+          Ingredients: {initialEntree.ingredients.length}
+        </h3>
         <div className="button-group">
           <button
             className="pass-button entreeButton"
-            onClick={handlePassIncriment}
+            onClick={handlePassIncrement}
           >
             &#10005;
           </button>
           <button
             className="like-button entreeButton"
-            onClick={handleLikeIncriment}
+            onClick={handleLikeIncrement}
           >
             &#10004;
           </button>
